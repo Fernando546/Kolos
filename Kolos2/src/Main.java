4ff31a8ae639ca5f53616cae323902ad1eec42df
Dawid <ferero546@gmail.com>
2023-11-28 18:17:25 +0100

    Update Main.java

--- Kolos2/src/Main.java
+++ Kolos2/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
             tab[i] = ram.nextInt(73) + 17;
         }
         int bat[] = tab.clone();
-        for (int i = 0; i < tab.length; i++) {
+        for (int i = 0; i < bat.length; i++) {
             System.out.println(bat[i]);
         }
     }
