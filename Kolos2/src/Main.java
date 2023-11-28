b5d20dbf4de16c045990fab81eb3c73a9f14dfa5
Dawid <ferero546@gmail.com>
2023-11-28 18:16:17 +0100

    Update Main.java

--- Kolos2/src/Main.java
+++ Kolos2/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Random ram = new Random();
         int tab[] = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < tab.length; i++) {
             tab[i] = ram.nextInt(73) + 17;
         }
         int bat[] = tab.clone();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < tab.length; i++) {
             System.out.println(bat[i]);
         }
     }
